d608befc761d310a06bace37b6ad9437a748aa68
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-05-05 13:43:35 -0500

    Create function foo (declaration) that returns an int result.

--- toy01.cpp
+++ toy01.cpp
@@ -1,3 +1,4 @@
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 
@@ -6,6 +7,14 @@ static LLVMContext Context;
 static Module *ModuleOb = new Module("My compiler",Context);
 
 int main(int argc, char *argv[]) {
+    static IRBuilder<> Builder = IRBuilder<>(Context);
+
+    // create a function "foo" that returns int result
+    Function *fooFunc;
+    FunctionType *fooFuncType = FunctionType::get(Builder.getInt32Ty(),false);
+    fooFunc = Function::Create(fooFuncType, Function::ExternalLinkage, "foo", ModuleOb);
+
+
     ModuleOb->dump();
     return 0;
 }
