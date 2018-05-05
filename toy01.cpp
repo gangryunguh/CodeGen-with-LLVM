9dbc92a9d5938d7d500bfc06febea782a72a7e27
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-05-05 14:37:10 -0500

    Add a basic block entry to the function foo.

--- toy01.cpp
+++ toy01.cpp
@@ -1,4 +1,5 @@
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Verifier.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 
@@ -14,6 +15,10 @@ int main(int argc, char *argv[]) {
     FunctionType *fooFuncType = FunctionType::get(Builder.getInt32Ty(),false);
     fooFunc = Function::Create(fooFuncType, Function::ExternalLinkage, "foo", ModuleOb);
 
+    // adding a block to a function "foo"
+    BasicBlock *entry = BasicBlock::Create(Context,"entry",fooFunc);
+    Builder.SetInsertPoint(entry);
+    verifyFunction(*fooFunc);
 
     ModuleOb->dump();
     return 0;
